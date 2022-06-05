5a1976860b9553eed4e17544f664057d09787786
wjrdjs005 <wjrdjs005@naver.com>
2022-06-05 23:21:59 +0900

    AddTodo TodoList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
+import AddTodo from "./components/Todo/AddTodo";
+import TodoList from "./components/Todo/TodoList";
 
 function App() {
   
   return (
     <Fragment>
-
+      <AddTodo />
+      <TodoList />
     </Fragment>
   )
 }
